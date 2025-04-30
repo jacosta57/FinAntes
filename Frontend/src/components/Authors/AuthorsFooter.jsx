07364699444f3d1c4c89ea6367b148ca3055df44
Rachman, Zaid <zaidr@iastate.edu>
2025-04-30 10:41:43 -0500

    Completed Frontend of Authors Page

---
+++ Frontend/src/components/Authors/AuthorsFooter.jsx
@@ -0,0 +1,18 @@
+function AuthorsFooter(){
+    return(
+        <div
+            style={{
+                position: 'fixed',
+                bottom: '0px',
+                backgroundColor:'#2962FF',
+                width: '100vw',
+                color: 'white',
+                textAlign: "center",
+                padding: '10 px',
+            }}
+                >      
+            <p>SE/COMS3190 - Construction of User Interfaces, Spring 2025</p>
+        </div>
+    )
+}
+export default AuthorsFooter;
---
+++ Frontend/src/components/Authors/AuthorsHeader.jsx
@@ -0,0 +1,9 @@
+function AuthorsHeader(){
+    return(
+        <div>
+            <h1 style={{backgroundColor:'#2962FF'}} className="text-4xl font-bold text-center text-white pb-2 m-auto"
+            >Team Information Page</h1>
+        </div>/*#2962FF*/
+    )
+}
+export default AuthorsHeader;
